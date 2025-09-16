664d030f61edf70e49c271d981dc216baa399353
Aayush Shrestha <learn.aayush@gmail.com>
2025-09-17 00:00:33 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+// let isJsProgramminglanguage = true;
+// let isJsHard = false;
+// let favnum = 20;
+// console.log(isJsProgramminglanguage);
+// console.log(isJsHard);
+// console.log(favnum);
+
+
+//switch case
+
+// let x=5;
+// let txt;
+// switch (x){
+//     case 0 :
+//         text = "off"
+//         break;
+//     case 1 :
+//         text= "sunday";
+//         break;
+//     case 2 :
+//         text= "Monday";
+//         break;
+//     case 3 :
+//         text= "tuesday";
+//         break;
+//     case 4 :
+//         text= "wednesday";
+//         break;
+//     case 5 :
+//         text= "thursday";
+//         break;
+//     case 6 :
+//         text= "friday";
+//         break;
+//     case 5 :
+//         text= "saturday";
+//         break;    
+//     default:  
+//     console.log("there are seven days in a week");   
+
+// }
+
+// console.log (text);
+
+
+// Array
+
+const favSinger=["nabin k bhattrai", "Ishan onta" , "Sabin Rai"];
+console.log(favSinger[0]);
+
+const favNum = [1,2,3,4];
+const mixedarr =   ["aayush",["gurjudhara","nepal"],123, true];
+console.log(favNum);
+console.log(mixedarr);
